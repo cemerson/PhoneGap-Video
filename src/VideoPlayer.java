74c628dca534ccb347f641a4ace4c1a1e00ade1c
Chris Emerson <cemerson@perform-impact.com>
2013-09-30 07:36:09 -0400

    20130930.1

--- src/VideoPlayer.java
+++ src/VideoPlayer.java
@@ -5,37 +5,37 @@
  * Copyright (c) 2005-2010, Nitobi Software Inc.
  * Copyright (c) 2011, IBM Corporation
  */
- 
+
 package com.phonegap.plugins.video;
- 
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
- 
+
 import org.json.JSONArray;
 import org.json.JSONException;
- 
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
- 
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
- 
+
 public class VideoPlayer extends CordovaPlugin {
     private static final String YOU_TUBE = "youtube.com";
     private static final String ASSETS = "file:///android_asset/";
- 
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
         PluginResult.Status status = PluginResult.Status.OK;
         String result = "";
- 
+
         try {
             if (action.equals("playVideo")) {
                 playVideo(args.getString(0));
@@ -51,7 +51,7 @@ public class VideoPlayer extends CordovaPlugin {
         }
         return true;
     }
- 
+
     private void playVideo(String url) throws IOException {
         if (url.contains("bit.ly/") || url.contains("goo.gl/") || url.contains("tinyurl.com/") || url.contains("youtu.be/")) {
             //support for google / bitly / tinyurl / youtube shortens
@@ -62,10 +62,10 @@ public class VideoPlayer extends CordovaPlugin {
             url = con.getURL().toString();
             is.close();
         }
-         
+
         // Create URI
         Uri uri = Uri.parse(url);
- 
+
         Intent intent = null;
         // Check to see if someone is trying to play a YouTube page.
         if (url.contains(YOU_TUBE)) {
@@ -78,20 +78,21 @@ public class VideoPlayer extends CordovaPlugin {
                 intent.setData(Uri.parse("market://details?id=com.google.android.youtube"));
             }
         } else if(url.contains(ASSETS)) {
+
             // get file path in assets folder
             String filepath = url.replace(ASSETS, "");
             // get actual filename from path as command to write to internal storage doesn't like folders
             String filename = filepath.substring(filepath.lastIndexOf("/")+1, filepath.length());
- 
+
             // Don't copy the file if it already exists
             File fp = new File(this.cordova.getActivity().getFilesDir() + "/" + filename);
             if (!fp.exists()) {
                 this.copy(filepath, filename);
             }
- 
+
             // change uri to be to the new file in internal storage
             uri = Uri.parse("file://" + this.cordova.getActivity().getFilesDir() + "/" + filename);
- 
+
             // Display video player
             intent = new Intent(Intent.ACTION_VIEW);
             intent.setDataAndType(uri, "video/*");
@@ -100,17 +101,17 @@ public class VideoPlayer extends CordovaPlugin {
             intent = new Intent(Intent.ACTION_VIEW);
             intent.setDataAndType(uri, "video/*");
         }
- 
+
         this.cordova.getActivity().startActivity(intent);
     }
- 
+
     private void copy(String fileFrom, String fileTo) throws IOException {
         // get file to be copied from assets
         InputStream in = this.cordova.getActivity().getAssets().open(fileFrom);
         // get file where copied too, in internal storage.
         // must be MODE_WORLD_READABLE or Android can't play it
         FileOutputStream out = this.cordova.getActivity().openFileOutput(fileTo, Context.MODE_WORLD_READABLE);
- 
+
         // Transfer bytes from in to out
         byte[] buf = new byte[1024];
         int len;
@@ -119,7 +120,7 @@ public class VideoPlayer extends CordovaPlugin {
         in.close();
         out.close();
     }
-     
+
     private boolean isYouTubeInstalled() {
         PackageManager pm = this.cordova.getActivity().getPackageManager();
         try {
